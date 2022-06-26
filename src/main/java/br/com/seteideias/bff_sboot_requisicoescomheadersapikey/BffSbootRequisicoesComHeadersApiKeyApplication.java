4818be3d0bc1242170fcf32d909bd3fc99dc4cc9
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-06-26 18:33:19 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/bff_sboot_requisicoescomheadersapikey/BffSbootRequisicoesComHeadersApiKeyApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.bff_sboot_requisicoescomheadersapikey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSbootRequisicoesComHeadersApiKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffSbootRequisicoesComHeadersApiKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/seteideias/bff_sboot_requisicoescomheadersapikey/BffSbootRequisicoesComHeadersApiKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.bff_sboot_requisicoescomheadersapikey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSbootRequisicoesComHeadersApiKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
